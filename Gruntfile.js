b56b6c336e979bac2116550da079880024a66482
m m <m0ritz.m3yer@gmail.com>
2014-04-27 14:53:00 +0200

    chore(build): remove connect complete from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'uglify:dist', 'uglify:src']);
